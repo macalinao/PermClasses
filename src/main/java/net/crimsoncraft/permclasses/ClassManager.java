15d899f6b5f559d11fd964ac1895bed217d88dd0
Ian Macali <ianmacalinao@gmail.com>
2012-03-23 18:26:50 -0500

    Added getting of player classes.

--- src/main/java/net/crimsoncraft/permclasses/ClassManager.java
+++ src/main/java/net/crimsoncraft/permclasses/ClassManager.java
@@ -23,7 +23,9 @@
  */
 package net.crimsoncraft.permclasses;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
@@ -34,11 +36,15 @@ import org.bukkit.plugin.RegisteredServiceProvider;
  */
 public class ClassManager {
 
+    private Map<String, ClassType> classTypes = new HashMap<String, ClassType>();
+
     private Map<String, PermClass> classes = new HashMap<String, PermClass>();
+
+    private String classPrefix = "pcl_";
 
     private PermClasses plugin;
 
-    private net.milkbowl.vault.permission.Permission permissionsHook;
+    private net.milkbowl.vault.permission.Permission permAPI;
 
     public ClassManager(PermClasses plugin) {
         this.plugin = plugin;
@@ -51,13 +57,43 @@ public class ClassManager {
     private void setupPermissions() {
         RegisteredServiceProvider<net.milkbowl.vault.permission.Permission> permissionProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
         if (permissionProvider != null) {
-            permissionsHook = permissionProvider.getProvider();
+            permAPI = permissionProvider.getProvider();
         }
 
-        if (permissionsHook == null) {
+        if (permAPI == null) {
             plugin.getLogger().log(Level.SEVERE, "No permissions plugin detected! This is pointless! Disabling the plugin.");
             Bukkit.getPluginManager().disablePlugin(plugin);
         }
+    }
+
+    public List<ClassType> getClassTypes() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Gets the classes of a player.
+     *
+     * @param player The player to get the classes of.
+     * @return The player's classes.
+     */
+    public Map<ClassType, PermClass> getClasses(String player) {
+        String[] groups = permAPI.getPlayerGroups(Bukkit.getWorlds().get(0), player);
+        Map<ClassType, PermClass> classMap = new HashMap<ClassType, PermClass>();
+        for (String group : groups) {
+            if (!group.startsWith("pcl_")) {
+                continue;
+            }
+
+            for (ClassType type : getClassTypes()) {
+                if (!group.startsWith("pcl_" + type.getId())) {
+                    continue;
+                }
+
+                int index = type.getId().length() + classPrefix.length() - 1;
+                String classId = group.substring(index);
+            }
+        }
+        return classMap;
     }
 
 }
--- src/main/java/net/crimsoncraft/permclasses/ClassType.java
+++ src/main/java/net/crimsoncraft/permclasses/ClassType.java
@@ -9,5 +9,23 @@ package net.crimsoncraft.permclasses;
  * @author ianschool
  */
 public class ClassType {
+    private String name;
+    
+    public ClassType(String name) {
+        
+    }
+    
+    public String getId() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    /**
+     * Gets the name of this class type.
+     * 
+     * @return The name of this ClassType.
+     */
+    public String getName() {
+        return name;
+    }
     
 }
