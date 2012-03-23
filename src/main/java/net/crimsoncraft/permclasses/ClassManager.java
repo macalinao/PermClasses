6de767ef297023ffad4b57041580e3fb43ff3381
Ian M <ianmacalinao@gmail.com>
2012-03-23 12:18:19 -0500

    Perm classes and class types.

---
+++ src/main/java/net/crimsoncraft/permclasses/ClassManager.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of PermClasses.
+ * 
+ * PermClasses is released under the Voxton License version 1.
+ *
+ * PermClasses is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition to this, you must also specify that this product includes 
+ * software developed by Voxton.net and may not remove any code
+ * referencing Voxton.net directly or indirectly.
+ * 
+ * PermClasses is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and the Voxton license along with PermClasses. 
+ * If not, see <http://voxton.net/voxton-license-v1.txt>.
+ */
+package net.crimsoncraft.permclasses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages classes.
+ */
+public class ClassManager {
+
+    private Map<String, PermClass> classes = new HashMap<String, PermClass>();
+    private PermClasses plugin;
+
+    public ClassManager(PermClasses plugin) {
+        this.plugin = plugin;
+    }
+}
---
+++ src/main/java/net/crimsoncraft/permclasses/ClassType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.crimsoncraft.permclasses;
+
+/**
+ *
+ * @author ianschool
+ */
+public class ClassType {
+    
+}
