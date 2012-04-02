a029ca30b0c4cf1b5b96fc39ff2bf4be604dd197
Ian Macali <ianmacalinao@gmail.com>
2012-04-01 23:26:09 -0500

    Switch over to AGPL. It's simpler.

--- src/main/java/net/crimsoncraft/permclasses/PermClasses.java
+++ src/main/java/net/crimsoncraft/permclasses/PermClasses.java
@@ -1,25 +1,18 @@
 /*
  * This file is part of PermClasses.
- * 
- * PermClasses is released under the Voxton License version 1.
  *
  * PermClasses is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * In addition to this, you must also specify that this product includes 
- * software developed by Voxton.net and may not remove any code
- * referencing Voxton.net directly or indirectly.
- * 
  * PermClasses is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * and the Voxton license along with PermClasses. 
- * If not, see <http://voxton.net/voxton-license-v1.txt>.
+ * along with PermClasses.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.crimsoncraft.permclasses;
 
